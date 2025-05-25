8ca70fda48544f62f4e15ed73b224c0b8ae8471a
Yalgrin <yalgrin@yalgrin.pl>
2025-05-25 11:54:21 +0200

    Removed the said debug tool.

--- src/main/java/pl/yalgrin/playnite/simplesync/PlayniteSimpleSyncServerApplication.java
+++ src/main/java/pl/yalgrin/playnite/simplesync/PlayniteSimpleSyncServerApplication.java
@@ -2,13 +2,11 @@ package pl.yalgrin.playnite.simplesync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class PlayniteSimpleSyncServerApplication {
 
     public static void main(String[] args) {
-        ReactorDebugAgent.init();
         SpringApplication.run(PlayniteSimpleSyncServerApplication.class, args);
     }
 
